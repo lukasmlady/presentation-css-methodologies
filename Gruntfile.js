76101725b99a77b00bc3e48ce13d74c29d206611
Lukáš Mladý <mladylukas@gmail.com>
2015-04-05 22:49:46 +0200

    feat: add common plugins and stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,7 +47,9 @@ module.exports = function(grunt) {
 					'css/theme/sky.css': 'css/theme/source/sky.scss',
 					'css/theme/moon.css': 'css/theme/source/moon.scss',
 					'css/theme/solarized.css': 'css/theme/source/solarized.scss',
-					'css/theme/blood.css': 'css/theme/source/blood.scss'
+					'css/theme/blood.css': 'css/theme/source/blood.scss',
+
+					'css/theme/custom.css': 'css/theme/source/custom.scss'
 				}
 			}
 		},
